60b5f973306fc58ff17df6179c7cbc31b21617fa
Mart Pluijmaekers <mart@tuupke.nl>
2016-03-23 01:40:06 +0100

    Adding progress, first stub in place and most things work, minor iterations and show of message left

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,13 +1,20 @@
 var hamb = document.querySelector('#hamburger');
 var menu = document.querySelector("#main");
-console.log(hamb, menu);
 
-var active = false;
+active = false;
 
 hamb.onclick=function(e){
     console.log('Click op hanmb');
     active = !active;
     menu.setAttribute('class', active ? 'show' : '');
+    window.scrollTo(0,0);
+
 };
+
+//document.body.onscroll = function(){
+//    if(!window.active){
+//        window.scrollTo(0,0);
+//    }
+//};
 
 hamb.style.marginLeft = "0px";
