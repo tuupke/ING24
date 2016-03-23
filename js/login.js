84a9a7b965448161ccb23cb33e2658869bde9dfd
Jeroen31 <j.v.oorschot@student.tue.nl>
2016-03-23 05:25:14 +0100

    vorige keer vergeten, oepserdepoeps

--- js/login.js
+++ js/login.js
@@ -2,7 +2,11 @@ var login = document.querySelector('#login');
 var x=0;
 
 login.onclick=function(e){
+    if(x!=4){
       document.getElementById("login"+x).style.display = 'none';
       x+=1;
+      }else{
+        document.getElementById("login").style.display = 'none';
+      }
 };
 
